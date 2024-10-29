9ba7579b595b09deab6a395236b4ec76de9d4b4c
Sean Dever <sean.dever@datadoghq.com>
2024-10-29 16:29:05 -0700

    adding additional notes

--- string_input/str_input.c
+++ string_input/str_input.c
@@ -5,7 +5,8 @@ int main() {
     char name[100]; //declaring a string with the size of 100 characters. 
 
     printf("Enter name:\n");
+    //The 100 in the scanf function is the maximum number of characters that can be read from the input.
+    //so if the user enters more than 100 characters, the extra characters will be ignored.
     scanf("%100s", name); //taking input from user.
-
     printf("Hello: %s\n", name); //printing the input.
 }
