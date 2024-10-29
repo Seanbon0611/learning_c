85f16ff6b47689f477d83d2e90f8a5ae0cf1eff4
Sean Dever <sean.dever@datadoghq.com>
2024-10-29 16:45:33 -0700

    adding line input example

--- line_input/line_input.c
+++ line_input/line_input.c
@@ -4,5 +4,7 @@ int main() {
     char line[1000];
     printf("Enter line\n");
     scanf("%[^\n]1000s", line); // read until newline or 1000 characters
+    //alternatively, you can use fgets(line, sizeof(line), stdin);
+    // fgets(line, 10000, stdin);
     printf("You entered: %s\n", line);
 }
